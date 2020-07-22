f9e76eac693c543f6b5c33d84ee07f81f1929279
Soeren Helbig <soeren.helbig@projektmotor.de>
2020-07-22 22:48:11 +0200

    * fixes docker version retrieval * updates docker compose

--- install-docker.sh
+++ install-docker.sh
@@ -2,11 +2,43 @@
 
 BASE_URL="https://download.docker.com/linux/static/stable/x86_64/"
 DIRECTORY_LISTING=$(curl -s ${BASE_URL} | awk 'BEGIN{ RS="<a *href *= *\""} NR>2 {sub(/".*/,"");print; }')
-DOCKER_COMPOSE_URL="https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m`"
+DOCKER_COMPOSE_URL="https://github.com/docker/compose/releases/download/1.26.2/docker-compose-`uname -s`-`uname -m`"
 RELEASES=(${DIRECTORY_LISTING// / })
-CURRENT_RELEASE=${RELEASES[-1]}
-CURRENT_RELEASE_URL="${BASE_URL}${CURRENT_RELEASE}"
 TMP_DIR="$HOME/.tmp-docker-install-files"
+
+MAJOR=0
+MINOR=0
+PATCH=0
+
+for i in ${RELEASES[*]}
+do
+        FILTERED_FILENAME=$(echo $i | egrep 'docker\-[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.tgz')
+
+        if [[ "${FILTERED_FILENAME}" == "" ]]; then
+                continue
+        fi
+
+        TMP_MAJOR=$(echo ${FILTERED_FILENAME} |  awk -F'[.-]' '{print $2}')
+        TMP_MINOR=$(echo ${FILTERED_FILENAME} |  awk -F'[.-]' '{print $3}')
+        TMP_PATCH=$(echo ${FILTERED_FILENAME} |  awk -F'[.-]' '{print $4}')
+
+        if [ ${TMP_MAJOR} -gt ${MAJOR} ]; then
+                MAJOR=${TMP_MAJOR}
+                MINOR=0
+                PATCH=0
+        fi
+
+        if [ ${TMP_MINOR} -gt ${MINOR} ]; then
+                MINOR=${TMP_MINOR}
+                PATCH=0
+        fi
+
+        if [ ${TMP_PATCH} -gt ${PATCH} ]; then
+                PATCH=${TMP_PATCH}
+        fi
+done
+
+CURRENT_RELEASE_URL="${BASE_URL}/docker-${MAJOR}.${MINOR}.${PATCH}.tgz"
 
 mkdir ${TMP_DIR}
 cd ${TMP_DIR}
